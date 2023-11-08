35bcd79e94d7c081494bfd1352649550b8f44e4b
Joaquin Escobar <escobarjoaquinc@gmail.com>
2023-11-08 01:10:00 -0300

    segunda pre entrega, ordenando codigo

--- segunda pre-entrega/src/app.js
+++ segunda pre-entrega/src/app.js
@@ -1,13 +1,8 @@
 import express from 'express'
-import studentsRouter from './router/studens.router.js'
-import coursesRouter from './router/courses.router.js'
-import viewsRouter from './router/views.router.js'
 import handlebars from 'express-handlebars'
 import mongoose from 'mongoose'
 import {__dirname} from './utils.js'
-
-/* /* import productsRouter from './router/products.router.js'; */
-import cartsRouter from './router/carts.router.js';
+import viewsRouter from './router/views.router.js'
 
 const app = express();
 
@@ -20,8 +15,7 @@ app.set('view engine', 'handlebars');
 
 
 app.use('/', viewsRouter);
-app.use('/api/students', studentsRouter);
-app.use('/api/courses', coursesRouter);
+
 
 try {
     await mongoose.connect('mongodb+srv://escobarjoaquin2002:okvGqraOuslIpkBi@clustercoderback.osj8i4p.mongodb.net/proyect?retryWrites=true&w=majority');
